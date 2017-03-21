ed67faaf0e16359a32f4801b175a3e9ce6107915
Vbubblery <juncheng.zhou@u-psud.fr>
2017-03-22 00:41:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,16 @@ mongoose.connection.on('error', function() {
   console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?'.red);
 });
 
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 app.set('port', process.env.PORT || 3001);
+app.use(allowCrossDomain);
 app.use(compression());
 app.use(logger('dev'));
 app.use(bodyParser.json());
