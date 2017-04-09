1f3631ff3124d12e3a7bde2dfaada11ba85eb9fe
Vbubblery <juncheng.zhou@u-psud.fr>
2017-04-09 18:58:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ mongoose.connection.on('error', function() {
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 }
 
